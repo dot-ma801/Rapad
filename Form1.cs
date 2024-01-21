5790c1c6de1338f827ba6403456792c2c30d4a6f
dot-ma801 <dot.ma801@gmail.com>
2024-01-21 20:58:32 +0900

    [clean] #21 使用していない`using`を削除

--- Form1.cs
+++ Form1.cs
@@ -1,16 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Threading;
 using System.IO;
 using System.Reflection;
-using static System.Windows.Forms.VisualStyles.VisualStyleElement;
 
 namespace Rapad
 {
